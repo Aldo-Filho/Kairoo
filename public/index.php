a0353d84cf2ef241df3ada70858667cef93e2c9f
Aldo-Filho <aldinhuuu145@gmail.com>
2025-11-27 13:01:05 -0300

    Subindo Arquivos

---
+++ public/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kairoo | Início</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/header.css">
+    <link rel="stylesheet" href="assets/css/index.css">
+</head>
+<body>
+    <nav class="justify-content-between d-flex align-items-center p-3">
+        <a href="#" class="">
+            <img src="assets/img/leakedLogo.png" class="logoImg">
+        </a>
+        <div class="d-flex align-items-center">
+            <ul class="nav d-flex align-items-center gap-4">
+                <li>
+                    <a href="#" class="text-decoration-none">INICIO</a>
+                </li>
+
+                <li>
+                    <a href="#" class="text-decoration-none">SOBRE</a>
+                </li>
+
+                <li>
+                    <a href="#" class="text-decoration-none" >HISTORIA</a>
+                </li>
+
+                <li>
+                    <a href="#" class="text-decoration-none" >PERSONAGENS</a>
+                </li>
+            </ul>
+        </div>
+        <a class="border border-3 border-white rounded-pill d-flex align-items-center border p-1 px-3" href="">
+            ENTRAR
+        </a>
+    </nav>
+
+    <main>
+        <section>
+
+        </section>
+        <section>
+            
+        </section>
+    </main>
+
+</body>
+</html>
